601067d15370c062696c545349c1775c3e9c990e
Abdelrahman-Muhammad <97410751+Abdelrahman-Muhammad@users.noreply.github.com>
2023-05-24 23:35:25 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -9,13 +9,13 @@
  */
 void read_user_input(char *user_input)
 {
-    printf("#cisfun$ ");
-    if (fgets(user_input, MAX, stdin) == NULL)
-    {
-        printf("\n");
-        exit(EXIT_SUCCESS);
-    }
-    user_input[strcspn(user_input, "\n")] = '\0';
+	printf("#cisfun$ ");
+	if (fgets(user_input, MAX, stdin) == NULL)
+	{
+		printf("\n");
+		exit(EXIT_SUCCESS);
+	}
+	user_input[strcspn(user_input, "\n")] = '\0';
 }
 
 /**
@@ -25,51 +25,51 @@ void read_user_input(char *user_input)
  */
 void execute_command(char *command, const char *program_name)
 {
-    pid_t pid;
+	pid_t pid;
 
-    char **args = malloc(2 * sizeof(char *));
-    if (args == NULL)
-    {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
+	char **args = malloc(2 * sizeof(char *));
+	if (args == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
-    args[0] = command;
-    args[1] = NULL;
+	args[0] = command;
+	args[1] = NULL;
 
-    if ((pid = fork()) == -1)
-    {
-        perror("fork");
-        free(args);
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        execve(command, args, NULL);
-        perror(program_name);
-        free(args);
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        int child_state;
-        if (wait(&child_state) == -1)
-        {
-            perror("wait");
-            free(args);
-            exit(EXIT_FAILURE);
-        }
-        if (WIFEXITED(child_state) && WEXITSTATUS(child_state) == 0)
-        {
-            free(args);
-            return;
-        }
-        else
-        {
-            free(args);
-            return;
-        }
-    }
+	if ((pid = fork()) == -1)
+	{
+		perror("fork");
+		free(args);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		execve(command, args, NULL);
+		perror(program_name);
+		free(args);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		int child_state;
+		if (wait(&child_state) == -1)
+		{
+			perror("wait");
+			free(args);
+			exit(EXIT_FAILURE);
+		}
+		if (WIFEXITED(child_state) && WEXITSTATUS(child_state) == 0)
+		{
+			free(args);
+			return;
+		}
+		else
+		{
+			free(args);
+			return;
+		}
+	}
 }
 
 /**
@@ -79,53 +79,53 @@ void execute_command(char *command, const char *program_name)
  */
 int main(int argc, char *argv[])
 {
-    char *user_input = NULL;
-    size_t input_size = 0;
+	char *user_input = NULL;
+	size_t input_size = 0;
 
-    if (argc == 1)
-    {
-        while (1)
-        {
-            if (getline(&user_input, &input_size, stdin) != -1)
-            {
-                user_input[strlen(user_input) - 1] = '\0';
-                execute_command(user_input, argv[0]);
-            }
-            else
-            {
-                if (feof(stdin))
-                    break;
-                else
-                {
-                    perror("getline");
-                    exit(EXIT_FAILURE);
-                }
-            }
-        }
-    }
-    else if (argc == 2)
-    {
-        FILE *file = fopen(argv[1], "r");
-        if (file == NULL)
-        {
-            perror("fopen");
-            exit(EXIT_FAILURE);
-        }
+	if (argc == 1)
+	{
+		while (1)
+		{
+			if (getline(&user_input, &input_size, stdin) != -1)
+			{
+				user_input[strlen(user_input) - 1] = '\0';
+				execute_command(user_input, argv[0]);
+			}
+			else
+			{
+				if (feof(stdin))
+					break;
+				else
+				{
+					perror("getline");
+					exit(EXIT_FAILURE);
+				}
+			}
+		}
+	}
+	else if (argc == 2)
+	{
+		FILE *file = fopen(argv[1], "r");
+		if (file == NULL)
+		{
+			perror("fopen");
+			exit(EXIT_FAILURE);
+		}
 
-        while (!feof(file) && getline(&user_input, &input_size, file) != -1)
-        {
-            user_input[strlen(user_input) - 1] = '\0';
-            execute_command(user_input, argv[0]);
-        }
+		while (!feof(file) && getline(&user_input, &input_size, file) != -1)
+		{
+			user_input[strlen(user_input) - 1] = '\0';
+			execute_command(user_input, argv[0]);
+		}
 
-        fclose(file);
-        free(user_input);
-    }
-    else
-    {
-        fprintf(stderr, "Usage: %s [script_file]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+		fclose(file);
+		free(user_input);
+	}
+	else
+	{
+		fprintf(stderr, "Usage: %s [script_file]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    return 0;
+	return 0;
 }
