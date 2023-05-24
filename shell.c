9a943a34dce3aa7ddb1b50cd2955d37a561511a0
Abdelrahman-Muhammad <97410751+Abdelrahman-Muhammad@users.noreply.github.com>
2023-05-24 14:23:13 +0300

    Update shell.c1

--- shell.c
+++ shell.c
@@ -5,78 +5,81 @@
 /**
  * read_user_input - Reads user input from the command line
  * @user_input: Pointer to store the user input
- */
-void read_user_input(char user_input[MAX])
+ */ 
+void read_user_input(char *user_input) 
 {
     printf("#cisfun$ ");
     if (fgets(user_input, MAX, stdin) == NULL)
     {
         printf("\n");
+        free(user_input);
         exit(0);
     }
     user_input[strcspn(user_input, "\n")] = '\0';
 }
 
-/**
- * execute_command - Executes the given command
- * @command: Command to execute
- * @program_name: Name of the program
- */
-void execute_command(const char *command, const char *program_name)
+/**  
+* execute_command - Executes the given command  
+* @command: Command to execute
+* @program_name: Name of the program
+*/
+void execute_command(char *command, const char *program_name) 
 {
-    pid_t pid = fork();
-
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(1);
+    char **args = malloc(2 * sizeof(char *));
+    if (args == NULL) {
+        perror("malloc");
+        exit(1);  
     }
-    else if (pid == 0)
-    {
-        char *args[2];
+    
+    args[0] = (char *)command; 
+    args[1] = NULL;  
+    
+    pid_t pid = fork();  
 
-        args[0] = (char *)command;
-        args[1] = NULL;
-
+    if (pid == -1) {
+        perror("fork");
+        free(args);
+        exit(1);
+    } else if (pid == 0) {
         execvp(command, args);
         perror(program_name);
+		free(args);
         exit(1);
-    }
-    else
-    {
-        int child_state;
-
-        if (wait(&child_state) == -1)
-        {
+    } else {    
+        int child_state;   
+        if (wait(&child_state) == -1) {
             perror("wait");
+            free(args); 
             exit(1);
         }
-        if (WIFEXITED(child_state) && WEXITSTATUS(child_state) == 0)
-        {
-            return;
-        }
-        else
-        {
+        if (WIFEXITED(child_state) && WEXITSTATUS(child_state) == 0) {
+            free(args);
+            return;     
+        } else {
+            free(args);
             return;
         }
     }
-}
+} 
 
 /**
- * main - Entry point of the shell program
- *
+ * main - Entry point of the shell program 
+ * 
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) 
 {
-    char user_input[MAX];
+    char *user_input = malloc(MAX);
+    if (user_input == NULL) {
+        perror("malloc");
+        exit(1);
+    }
     
     (void)argc;
-    while (1)
-    {
-        read_user_input(user_input);
-        execute_command(user_input, argv[0]);
+    while (1) {
+        read_user_input(user_input); 
     }
 
-    return (0);
+    free(user_input);
+    exit(0);
 }
