a63c72b348d5357a5a8f8e258bf9b88b24455b7c
Abdelrahman_Muhammad <boodyonline1@gmail.com>
2023-05-24 02:56:13 -0700

    5

--- shell.c
+++ shell.c
@@ -1,71 +1,68 @@
 #include "shell.h"
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+
 
 #define MAX 100
 
+void read_user_input(char user_input[MAX])
+{
+	printf("#cisfun$");
+	if (fgets(user_input, MAX, stdin) == NULL)
+	{
+		printf("\n");
+		exit(0);
+	}
+	user_input[strcspn(user_input, "\n")] = '\0';
+}
+
+void execute_command(char user_input[MAX])
+{
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+		printf("Error: %s\n", strerror(errno));
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		char *args[] = {user_input, NULL};
+
+		execve(user_input, args, __environ);
+		perror("Error");
+		exit(1);
+	}
+	else
+	{
+		int child_state;
+
+		if (wait(&child_state) == -1)
+		{
+			perror("Child Process Err");
+			exit(1);
+		}
+		if (WIFEXITED(child_state) && WEXITSTATUS(child_state) == 0)
+		{
+			return;
+		}
+		else
+		{
+			printf("%s: command Not Found\n", user_input);
+			return;
+		}
+	}
+}
+
 int main(void)
+
 {
 	char user_input[MAX];
 
 	while (1)
 	{
-		printf("#cisfun$");
-
-		if (fgets(user_input, MAX, stdin) == NULL)
-		{
-			printf("\n");
-            
-			exit(0);
-		}
-		user_input[strcspn(user_input, "\n")] = '\0';
-
-		pid_t pid = fork();
-
-		if (pid == -1)
-
-		{
-			perror("fork");
-
-            printf("Error: %s\n", strerror(errno));
-
-			exit(1);
-		}
-		else if (pid == 0)
-		{
-			char *args[] = {user_input,NULL};
-
-			execve(user_input, args,__environ);
-
-			perror("Error");
-
-			exit(1);
-		}
-		else
-		{
-			int child_state;
-			if (wait(&child_state) == -1)
-			{
-				perror("Chile Process Err");
-				exit(1);
-			}
-
-			if (WIFEXITED(child_state) && WEXITSTATUS(child_state) == 0)
-			{
-				continue;
-			}
-			else
-			{
-				printf("%s: cmnd Not Found\n", user_input);
-
-				continue;
-			}
-		}
-
+		read_user_input(user_input);
+		execute_command(user_input);
 	}
-
 	return (0);
 }
+
