47f9395cc6267c9523787a81b92165c1dd9cf554
Abdelrahman-Muhammad <97410751+Abdelrahman-Muhammad@users.noreply.github.com>
2023-05-24 13:38:23 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,11 +1,14 @@
 #include "shell.h"
 
-
 #define MAX 100
 
+/**
+ * read_user_input - Reads user input from the command line
+ * @user_input: Pointer to store the user input
+ */
 void read_user_input(char user_input[MAX])
 {
-    printf("#cisfun$");
+    printf("#cisfun$ ");
     if (fgets(user_input, MAX, stdin) == NULL)
     {
         printf("\n");
@@ -14,30 +17,38 @@ void read_user_input(char user_input[MAX])
     user_input[strcspn(user_input, "\n")] = '\0';
 }
 
-void execute_command(char user_input[MAX])
+/**
+ * execute_command - Executes the given command
+ * @command: Command to execute
+ * @program_name: Name of the program
+ */
+void execute_command(const char *command, const char *program_name)
 {
     pid_t pid = fork();
+
     if (pid == -1)
     {
         perror("fork");
-        printf("Error: %s\n", strerror(errno));
         exit(1);
     }
     else if (pid == 0)
     {
         char *args[2];
-        args[0] = user_input;
+
+        args[0] = (char *)command;
         args[1] = NULL;
-        execvp(user_input, args);
-        perror("Error");
+
+        execvp(command, args);
+        perror(program_name);
         exit(1);
     }
     else
     {
         int child_state;
+
         if (wait(&child_state) == -1)
         {
-            perror("Child Process Err");
+            perror("wait");
             exit(1);
         }
         if (WIFEXITED(child_state) && WEXITSTATUS(child_state) == 0)
@@ -46,19 +57,25 @@ void execute_command(char user_input[MAX])
         }
         else
         {
-            printf("%s: command Not Found\n", user_input);
             return;
         }
     }
 }
 
-int main(void)
+/**
+ * main - Entry point of the shell program
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
 {
     char user_input[MAX];
+
     while (1)
     {
         read_user_input(user_input);
-        execute_command(user_input);
+        execute_command(user_input, argv[0]);
     }
-    return 0;
+
+    return (0);
 }
